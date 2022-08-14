30d7a43502a2f44937768e64541d2dd88e07c1c2
zooyaho <jiwoo1268@naver.com>
2022-08-14 22:26:37 +0900

    Fix: Default Setting
    기본 세팅과 app.js에 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import AddBtn from "./AddBtn";
+import Header from "./Header";
+import TodoList from "./Todo/TodoList";
+import { useState } from "react";
 
 function App() {
+  const [] = useState(true);
+  const AddTodoHandler = () => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <TodoList />
+      <AddBtn onClick={AddTodoHandler} />
+    </>
   );
 }
 
