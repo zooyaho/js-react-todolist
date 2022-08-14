3c7403892f499f82252542e689836406201d5521
zooyaho <jiwoo1268@naver.com>
2022-08-14 22:24:35 +0900

    Style: Add header style
    header section 스타일 추가

---
+++ src/Header.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+const Header = () => {
+  const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    div {
+      width: 20rem;
+      margin-top: 2rem;
+      background-color: #fff;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 15px;
+      border: 1px solid black;
+    }
+    h1 {
+      font-size: 20px;
+      margin: 1rem 0;
+      cursor: default;
+    }
+  `;
+
+  return (
+    <Header>
+      <div>
+        <h1>TODO LIST</h1>
+      </div>
+    </Header>
+  );
+};
+export default Header;
