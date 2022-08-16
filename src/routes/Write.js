d07d238fc3588e95f95792c645c29ebc3db3fe45
zooyaho <jiwoo1268@naver.com>
2022-08-16 16:50:14 +0900

    Style: Add Write styling
    Write스타일 추가

--- src/routes/Write.js
+++ src/routes/Write.js
@@ -1,26 +1,13 @@
-import styled from "styled-components";
-import WriteForm from "../components/Wite/WriteForm";
+import WriteForm from "../components/Write/WriteForm";
+import Date from "../components/UI/Date";
+import Card from "../components/UI/Card";
 
 const Write = () => {
-  const Write = styled.div`
-    width: 30rem;
-    background-color: #fff;
-    position: absolute;
-    top: 40%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    border-radius: 20px;
-    p {
-      font-weight: bold;
-      font-size: 18px;
-      margin: 2rem 0 0 2.5rem;
-    }
-  `;
   return (
-    <Write>
-      <p>22.08.15</p>
+    <Card>
+      <Date date={"22.08.15"}></Date>
       <WriteForm />
-    </Write>
+    </Card>
   );
 };
 
