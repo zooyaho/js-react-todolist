8e29207a6fec185a596a906130b7231399ae8c62
zooyaho <jiwoo1268@naver.com>
2022-08-18 03:47:27 +0900

    Revert "Style: Change ContentWrap from p to div tag"
    This reverts commit a0330c22febfce0f20cc4f34c9688861974beaaf.

--- src/components/Layout/UpdateForm.js
+++ src/components/Layout/UpdateForm.js
@@ -1,29 +1,5 @@
-import styled from "styled-components";
 import { useRef } from "react";
-
-/* CSS */
-const UpdateFormSt = styled.form`
-  margin-top: 1.5rem;
-  width: 100%;
-  left: 50%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  input {
-    width: 80%;
-    padding: 0.7rem 0 0.7rem 1rem;
-    border-radius: 10px;
-    font-size: 16px;
-  }
-  textarea {
-    border-radius: 10px;
-    margin-top: 1rem;
-    padding-top: 1rem;
-    padding-left: 1rem;
-    width: 80%;
-    font-size: 16px;
-  }
-`;
+import Form from "./Form";
 
 const UpdateForm = (props) => {
   const titleInputRef = useRef();
@@ -38,8 +14,7 @@ const UpdateForm = (props) => {
   };
 
   return (
-    // onSubmit={submitHandler}
-    <UpdateFormSt onSubmit={submitHandler}>
+    <Form onSubmit={submitHandler}>
       <input
         ref={titleInputRef}
         type="text"
@@ -54,7 +29,7 @@ const UpdateForm = (props) => {
         defaultValue={props.content}
       />
       {props.children}
-    </UpdateFormSt>
+    </Form>
   );
 };
 export default UpdateForm;
--- src/components/Layout/WriteForm.js
+++ src/components/Layout/WriteForm.js
@@ -1,30 +1,6 @@
-import styled from "styled-components";
 import { useRef } from "react";
 import { v4 } from "uuid";
-
-/* CSS */
-const WriteFormSt = styled.form`
-  margin-top: 1.5rem;
-  width: 100%;
-  left: 50%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  input {
-    width: 80%;
-    padding: 0.7rem 0 0.7rem 1rem;
-    border-radius: 10px;
-    font-size: 16px;
-  }
-  textarea {
-    border-radius: 10px;
-    margin-top: 1rem;
-    padding-top: 1rem;
-    padding-left: 1rem;
-    width: 80%;
-    font-size: 16px;
-  }
-`;
+import Form from "./Form";
 
 const WriteForm = (props) => {
   const id = v4();
@@ -41,7 +17,7 @@ const WriteForm = (props) => {
   };
 
   return (
-    <WriteFormSt onSubmit={submitHandler}>
+    <Form onSubmit={submitHandler}>
       <input ref={titleInputRef} type="text" placeholder="title" />
       <textarea
         ref={contentInputRef}
@@ -50,7 +26,7 @@ const WriteForm = (props) => {
         placeholder="content"
       />
       {props.children}
-    </WriteFormSt>
+    </Form>
   );
 };
 export default WriteForm;
